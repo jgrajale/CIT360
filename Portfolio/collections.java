7e8d2f58c467e880b81859f57938b43a36b5493c
jgrajale <jgrajalesb@gmail.com>
2015-02-07 23:17:20 -0700

    Collections

---
+++ Portfolio/collections.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class JavaCollections {
+
+	public static void main(String[] args) {
+		
+		System.out.println( "Hello\n" ); 
+		
+		List list = new LinkedList();
+
+list.add("Networking");
+list.add("and Server Administration");
+list.add(0, "Cyber Security");
+
+	      System.out.println("My Name is Jonathan and I like: ");
+	      System.out.println(list);     
+					
+	}
+
+}
